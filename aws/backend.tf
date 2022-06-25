6c6182abced734c8249c8194e248d5a3163fef9c
Carlos Carvalho <carlos@chcdc.com.br>
2022-06-25 20:42:27 -0300

    chore:Removes AWS

--- aws/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "state-tf-aws-us-east1"
-    key    = ".terraform/"
-    region = "us-east-1"
-  }
-}
--- aws/main.tf
+++
@@ -1,4 +0,0 @@
-data "aws_s3_bucket_object" "ssh-key" {
-  bucket = "secrets-aws"
-  key    = "ssh-key.pem"
-}
