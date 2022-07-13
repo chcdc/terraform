cb33f30a06581fb1716da869284f6b4cc71994e0
Carlos Carvalho <carlos@chcdc.com.br>
2022-07-13 18:35:05 -0300

    fix(ci):Checkov requirements

--- azure/compute.tf
+++ azure/compute.tf
@@ -1,4 +1,3 @@
-
 resource "azurerm_network_interface" "nic" {
   name                      = "${var.machine}-Interface"
   location                  = var.location
@@ -9,38 +8,51 @@ resource "azurerm_network_interface" "nic" {
     name                          = "NICConfiguration"
     subnet_id                     = azurerm_subnet.InternalSubnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.PublicIP.id
   }
 }
 
-resource "azurerm_virtual_machine" "OSvm" {
-  name                          = var.machine
-  location                      = var.location
-  resource_group_name           = azurerm_resource_group.homolog.name
-  network_interface_ids         = ["${azurerm_network_interface.nic.id}"]
-  delete_os_disk_on_termination = true
-  vm_size                       = "Standard_B1s" #"Standard_DS1_v2"
+resource "azurerm_linux_virtual_machine" "OSvm" {
+  name           = var.machine
+  location       = var.location
+  admin_username = var.AdminUser
 
-  storage_os_disk {
-    name          = "OsDisk"
-    caching       = "ReadWrite"
-    create_option = "FromImage"
+  resource_group_name   = azurerm_resource_group.homolog.name
+  network_interface_ids = ["${azurerm_network_interface.nic.id}"]
+  vm_size               = "Standard_B1s"
+
+  allow_extension_operations    = false
+  delete_os_disk_on_termination = true
+
+  admin_ssh_key {
+    username   = var.AdminUser
+    public_key = file("~/.ssh/id_rsa.pub")
   }
 
-  storage_image_reference {
+  os_disk {
+    name                 = "OsDisk"
+    caching              = "ReadWrite"
+    create_option        = "FromImage"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "19.04"
     version   = "latest"
   }
 
-  os_profile {
-    computer_name  = var.machine
-    admin_username = var.AdminUser
-    admin_password = var.AdminPass
-  }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
+}
+
+resource "azurerm_virtual_machine_extension" "extension" {
+  name                       = "hostname"
+  virtual_machine_id         = azurerm_linux_virtual_machine.OSvm.id
+  publisher                  = "Microsoft.Azure.Security"
+  type                       = "IaaSAntimalware"
+  type_handler_version       = "2.0"
+  auto_upgrade_minor_version = true
 }
--- azure/nsg.tf
+++ azure/nsg.tf
@@ -6,35 +6,6 @@ resource "azurerm_network_security_group" "default_group" {
 }
 
 # Access Rules
-resource "azurerm_network_security_rule" "AllowSSH" {
-  name                        = "AllowSSH"
-  resource_group_name         = azurerm_resource_group.homolog.name
-  network_security_group_name = azurerm_network_security_group.default_group.name
-
-  priority                   = 100
-  access                     = "Allow"
-  direction                  = "Inbound"
-  protocol                   = "Tcp"
-  destination_port_range     = 22
-  destination_address_prefix = "*"
-  source_port_range          = "*"
-  source_address_prefix      = "*"
-}
-resource "azurerm_network_security_rule" "AllowHTTP" {
-  name                        = "AllowHTTP"
-  resource_group_name         = azurerm_resource_group.homolog.name
-  network_security_group_name = azurerm_network_security_group.default_group.name
-
-  priority                   = 102
-  access                     = "Allow"
-  direction                  = "Inbound"
-  protocol                   = "Tcp"
-  destination_port_range     = 80
-  destination_address_prefix = "*"
-  source_port_range          = "*"
-  source_address_prefix      = "*"
-}
-
 resource "azurerm_network_security_rule" "AllowHTTPS" {
   name                        = "AllowHTTPS"
   resource_group_name         = azurerm_resource_group.homolog.name
